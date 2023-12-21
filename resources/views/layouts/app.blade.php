4fc024b37de5ca9aa11eef07fe2195aae50dc9fb
SutoIstvan <isutoi@gmail.com>
2023-12-21 14:44:57 +0200

    add bootstrap.css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,7 @@
 
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3">
 
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  <link href="{{ asset('assets/css/bootstrap.css') }}" rel="stylesheet">
 
   <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
 
